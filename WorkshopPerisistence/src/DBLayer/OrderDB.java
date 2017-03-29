b543410ed7d946288446b5fec6e815b5cd967a95
SoetZombie <poortrolls@gmail.com>
2017-03-29 14:17:42 +0200

    order db

--- WorkshopPerisistence/src/DBLayer/OrderDB.java
+++ WorkshopPerisistence/src/DBLayer/OrderDB.java
@@ -1,15 +1,36 @@
 package DBLayer;
 
-import ModelLayer.Order;
+import  ModelLayer.Order;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
 import java.util.List;
 
+
 public class OrderDB implements OrderDBIF {
+    private Order order;
+    private Date createdate = order.getDate();
+    private Date deliveryDat = order.getDeliveryDate();
+
     @Override
     public Order create(Date date, int totalAmount, String deliveryStatus, Date deliveryDate, int invoiceId, int customerId) throws SQLException {
-        return null;
+        String sql = String.format("INSERT INTO Order (date, totalAmount, deliveryStatus, deliveryDate, invoiceId, customerId) VALUES "
+                + "'"+createdate+"'"+"('%d', '%s')"+"'"+deliveryDate+"'"+"('%d','%d')", date, totalAmount, deliveryStatus, deliveryDate, invoiceId, customerId);
+        try (Connection conn = DBConnection.getInstance().getDBcon();
+             Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(sql);
+        } catch(SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        Order o = new Order(date, totalAmount, deliveryStatus, deliveryDate, invoiceId, customerId);
+
+        return o;
+
+
     }
 
     @Override
