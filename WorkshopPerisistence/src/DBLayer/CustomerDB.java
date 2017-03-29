161e20e872c7dcc8a5016dce7a613cd913b70edf
Dimitar Stefanov <dsstefanov2@gmail.com>
2017-03-29 12:56:22 +0300

    DBLayer interfaces with related classes

--- WorkshopPerisistence/src/DBLayer/CustomerDB.java
+++ WorkshopPerisistence/src/DBLayer/CustomerDB.java
@@ -3,6 +3,7 @@ package DBLayer;
 import ModelLayer.Customer;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,5 +34,24 @@ public class CustomerDB implements CustomerDBIF{
     @Override
     public List<Customer> readAll() throws SQLException {
         return null;
+    }
+    private ArrayList<String> validate(String name, String address, int zip, boolean isCompany, String phoneNumber){
+        ArrayList<String> errorsBag = new ArrayList<>();
+        if (name.length()>50 || name.length()<1){
+            errorsBag.add("Name must be between 2 and 49 characters");
+        }
+        if (zip>=10000 && zip<1000){
+            errorsBag.add("Zip code must have 4 digit number");
+        }
+        if (phoneNumber.length()!=8){
+            errorsBag.add("Phone number must contain 8 digits");
+        }else{
+            try{
+                Integer.parseInt(phoneNumber);
+            }catch (Exception e){
+                errorsBag.add("Phone number must contain only numbers");
+            }
+        }
+        return (errorsBag.size()==0)?null:errorsBag;
     }
 }
