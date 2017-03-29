5763ddeaec0239faf8aaa48da50cc815ab0409e2
redjohn01 <john_4youu@yahoo.com>
2017-03-29 10:32:22 +0200

    Customer and region coded  in the model layer.

--- WorkshopPerisistence/src/ModelLayer/Customer.java
+++ WorkshopPerisistence/src/ModelLayer/Customer.java
@@ -4,4 +4,58 @@ package ModelLayer;
  * Created by Admin on 3/29/2017.
  */
 public class Customer {
+    private int id;
+    private String name;
+    private String address;
+    private int zip;
+    private boolean isCompany;
+    private String phoneNumber;
+
+    public Customer(String name, String address, int zip, boolean isCompany, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.zip = zip;
+        this.isCompany = isCompany;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public boolean isCompany() {
+        return isCompany;
+    }
+
+    public void setCompany(boolean company) {
+        isCompany = company;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
---
+++ WorkshopPerisistence/src/ModelLayer/Region.java
@@ -0,0 +1,30 @@
+package ModelLayer;
+
+/**
+ * Created by Red John on 29-Mar-17.
+ */
+public class Region {
+    private int zip;
+    private String city;
+
+    public Region(int zip, String city) {
+        this.zip = zip;
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
